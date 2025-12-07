1f178a73f38cf0ca803b3e044ed70d8d4dff588b
Miguel Ángel Moreno Campos <113242188+ezy0@users.noreply.github.com>
2025-12-07 19:20:27 +0100

    Update config.h

--- replay_attack_https/config.h
+++ replay_attack_https/config.h
@@ -1,2 +1,3 @@
-const char* ssid     = "MOVISTAR-WIFI6-8B38";//"vodafoneDA6B";//"MOVISTAR-WIFI6-8B38";
-const char* password = "c737FFAPC7JEAEE43HA4";//"BMFB9NNSTXTFPQ";//"c737FFAPC7JEAEE43HA4";
+const char* ssid     = "<ssid>";
+const char* password = "<pass>";
+
